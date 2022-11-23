33ec828a79ee7b226416db762e8420dd436cf7ff
maycon-santana <maycon.santana@thstec.com.br>
2022-11-23 20:52:14 -0300

    Crud de exemplo finalizado

--- projeto inicial/src/index.js
+++ projeto inicial/src/index.js
@@ -9,6 +9,33 @@ app.get('/projects', function(request, response) {
     ])
 })
 
+
+app.post('/projects', function(request, response) {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ])
+})
+
+app.put('/projects/:id', function(request, response) {
+    return response.json([
+        'Projeto 5',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ])
+})
+
+app.delete('/projects/:id', function(request, response) {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ])
+})
+
 app.listen(3000, () => {
     console.log('Server started on port 3000!')
 })
